9834ca93c4bbc105bf3f8c7deaf565358c4e13a8
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-15 13:40:44 +0200

    styling: navbar and it's animation

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "/src/style/Navbar.css";
+import { useState } from "react";
 
 export function Navbar(): JSX.Element {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBac = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBac);
   return (
-    <nav className="nav">
+    <nav className={navbar ? "nav active" : "nav"}>
       <img src="/src/image/Netflix.svg" alt="Netflix logo" className="logo" />
 
       <aside className="nav__wrapper">
