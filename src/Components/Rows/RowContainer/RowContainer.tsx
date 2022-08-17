b6d13afd88eb38ec3b5e3c291b043414bdafe45b
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-17 12:05:21 +0200

    creating the row('s) layout

--- src/Components/Rows/RowContainer/RowContainer.tsx
+++ src/Components/Rows/RowContainer/RowContainer.tsx
@@ -4,16 +4,22 @@ import Horror from "../Horror";
 import NetflixOriginals from "../NetflixOriginals";
 import Romance from "../Romance";
 import TopRated from "../TopRated";
+import requests from "../../requests";
+import UpComing from "../UpComing";
 
 export function RowContainer() {
   return (
     <div className="RowContainer">
-      <NetflixOriginals />
-      <TopRated />
-      <Action />
-      <Comedy />
-      <Romance />
-      <Horror />
+      <NetflixOriginals
+        title="Netflix Original"
+        fetchURL={requests.requestPopular}
+      />
+      <TopRated title="Top Rated" fetchURL={requests.requestTopRated} />
+      <UpComing title="Up Coming" fetchURL={requests.requestUpComing} />
+      <Action title="Action" fetchURL={requests.requestAction} />
+      <Comedy title="Comedy" fetchURL={requests.requestComedy} />
+      <Romance title="Romance" fetchURL={requests.requestRomance} />
+      <Horror title="Horror" fetchURL={requests.requestHorror} />
     </div>
   );
 }
