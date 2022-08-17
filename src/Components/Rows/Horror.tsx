695cb1ef087bf91c8d5553467f4b0b6cb2e92345
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-17 16:27:11 +0200

    fetching Horror movies and display them

--- src/Components/Rows/Horror.tsx
+++ src/Components/Rows/Horror.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
-export default function Horror() {
-  return <div>Horror</div>;
+type HorrorProps = {
+  title: string;
+  fetchURL: string;
+};
+
+export function Horror({ title, fetchURL }: HorrorProps) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(fetchURL)
+      .then((response) => response.json())
+      // 4. Setting *dogImage* to the image url that we received from the response above
+      .then((data) => {
+        setMovies(data.results);
+        console.log(movies);
+      });
+  }, []);
+  return (
+    <>
+      <h2 className="row__title">{title}</h2>
+      <div className="movies__wrapper">
+        <button className="scroll__btn--left">
+          <HiChevronLeft />{" "}
+        </button>
+        <div className="slider">
+          {movies.map((item, id) => (
+            <img
+              src={`https://image.tmdb.org/t/p/w500${item?.poster_path}`}
+              alt=""
+              key={id}
+            />
+          ))}
+        </div>
+
+        <button className="scroll__btn--right">
+          <HiChevronRight />
+        </button>
+      </div>
+    </>
+  );
 }
