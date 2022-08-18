8e23cfbd6560d854871353079e854774e13a4b6f
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-18 09:41:48 +0200

    refactor: change class name

--- src/Components/ShowCase/ShowCase.tsx
+++ src/Components/ShowCase/ShowCase.tsx
@@ -5,17 +5,17 @@ import { IoVolumeMediumOutline, IoVolumeOffOutline } from "react-icons/io5";
 import "/src/style/ShowCase.css";
 import ReactPlayer from "react-player";
 
-export function ShowCase() {
+export function ShowCase(): JSX.Element {
   const [isMuted, setIsMuted] = useState(true);
   return (
-    <div className="showcase">
-      <div className="wrapper">
+    <div className="banner">
+      <div className="banner__wrapper">
         <ReactPlayer
           playing={true}
           loop={true}
           volume={1}
           muted={isMuted}
-          className="video"
+          className="trailer__video"
           width="100%"
           height="100%"
           url="/src/Components/Assets/video/trailer.mp4"
@@ -30,21 +30,21 @@ export function ShowCase() {
           </button>
         )}
       </div>
-      <article className="movie">
-        <h1 className="movie__title">The Lion King </h1>
-        <p className="movie__summary">
+      <article className="trailer__info">
+        <h1 className="trailer__title">The Lion King </h1>
+        <p className="trailer__overview">
           Simba idolizes his father, King Mufasa, and takes to heart his own
           royal destiny. But not everyone in the kingdom celebrates the new
           cub's arrival. Scar, Mufasa's brother and former heir to the throne
           has plans of his own. The battle for Pride Rock is ravaged with
           betrayal, tragedy and drama, ultimately resulting in Simba's exile.
         </p>
-        <div className="movie__wrapper">
-          <button aria-label="Play">
+        <div className="trailer__container">
+          <button aria-label="Play" className="container__button">
             <FontAwesomeIcon icon={faPlay} className="icon" />
             &nbsp; Play
           </button>
-          <button aria-label="My List">
+          <button aria-label="My List" className="container__button">
             <FontAwesomeIcon icon={faPlus} className="icon" />
             &nbsp; My List
           </button>
