57bbf4bcb245db0b0e3dc43c7922e143e8cb2f05
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-20 10:30:17 +0200

    add overview text for mobile view

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -4,6 +4,7 @@ import { faPlay, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { IoVolumeMediumOutline, IoVolumeOffOutline } from "react-icons/io5";
 import "/src/style/Banner.css";
 import ReactPlayer from "react-player";
+import { BiInfoCircle } from "react-icons/bi";
 
 export function Banner(): JSX.Element {
   const [isMuted, setIsMuted] = useState(true);
@@ -19,6 +20,7 @@ export function Banner(): JSX.Element {
         height="100%"
         url="/src/Components/Assets/video/trailer.mp4"
       />
+
       {isMuted ? (
         <button onClick={() => setIsMuted(false)} className="mute__button">
           <IoVolumeOffOutline />
@@ -37,14 +39,23 @@ export function Banner(): JSX.Element {
           has plans of his own. The battle for Pride Rock is ravaged with
           betrayal, tragedy and drama, ultimately resulting in Simba's exile.
         </p>
+        <p className="trailer__overview mobile__view--1">
+          Simba idolizes his father, King Mufasa, and takes to heart his own
+          royal destiny. But not everyone in the kingdom celebrates the new
+          cub's arrival...
+        </p>
+        <p className="trailer__overview mobile__view--2">
+          Simba idolizes his father, King Mufasa, and takes to heart his own
+          royal destiny...
+        </p>
         <div className="trailer__container">
           <button aria-label="Play" className="container__button">
             <FontAwesomeIcon icon={faPlay} className="icon" />
             &nbsp; Play
           </button>
           <button aria-label="My List" className="container__button">
-            <FontAwesomeIcon icon={faPlus} className="icon" />
-            &nbsp; My List
+            <BiInfoCircle className="icon" />
+            &nbsp; More Info
           </button>
         </div>
       </article>
