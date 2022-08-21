27a4a5c6ea7e889d3ec8da6089b9b1e8de8d0b97
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-21 21:24:21 +0200

    fix bug: pass in key, refactor code: canhge class name and remove some elements

--- src/Components/Rows/NetflixOriginal/NetflixOriginals.tsx
+++ src/Components/Rows/NetflixOriginal/NetflixOriginals.tsx
@@ -1,24 +1,25 @@
-import { useEffect, useState, useRef } from "react";
-import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import { BsFillPlayFill } from "react-icons/bs";
-
-import {
-  FiPlus,
-  FiThumbsUp,
-  FiThumbsDown,
-  FiChevronDown,
-} from "react-icons/fi";
+import { useEffect, useState, useRef } from "react";
+import { FiPlus, FiChevronDown } from "react-icons/fi";
+import { VscChevronLeft, VscChevronRight } from "react-icons/vsc";
 
 import "/src/style/Rows.css";
 
-type NetflixOriginalsProps = {
+type NetflixOriginalProps = {
   title: string;
   fetchURL: string;
 };
 
-export function NetflixOriginals({ title, fetchURL }: NetflixOriginalsProps) {
+export function NetflixOriginals({ title, fetchURL }: NetflixOriginalProps) {
   const sliderRef = useRef<HTMLDivElement>(null);
-  const [movies, setMovies] = useState<string[]>([]);
+  const [movies, setMovies] = useState<
+    {
+      original_name: string;
+      id: number;
+      poster_path: string;
+      vote_average: number;
+    }[]
+  >([]);
 
   const slideLeft = () => {
     let slider = sliderRef.current;
@@ -35,52 +36,52 @@ export function NetflixOriginals({ title, fetchURL }: NetflixOriginalsProps) {
       // 4. Setting *dogImage* to the image url that we received from the response above
       .then((data) => {
         setMovies(data.results);
+        console.log(movies);
       });
   }, []);
 
   return (
-    <>
-      <h2 className="row__title Netflix">{title}</h2>
-      <div className="movie__container">
-        <button onClick={slideLeft} className="scroll__button scroll__left">
-          <HiChevronLeft />{" "}
-        </button>
-        <div className="slider" ref={sliderRef}>
-          {movies.map((item, id) => (
-            <div className="card">
-              <img
-                className="card__poster"
-                src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
-                alt=""
-                key={id}
-              />
-              <div className="movie__info">
-                <div className="movie__controls">
-                  <div className="movie__icons">
-                    <BsFillPlayFill /> <FiPlus /> <FiThumbsUp />{" "}
-                    <FiThumbsDown />
-                  </div>
-                  <div>
-                    <button className="movie__info--button">
-                      {" "}
-                      <FiChevronDown className="arrow-down" />{" "}
-                    </button>
-                  </div>
+    <div className="row">
+      <h2 className="row__title netflix">{title}</h2>
+      <button onClick={slideLeft} className="scroll__button scroll__left">
+        <VscChevronLeft />
+      </button>
+      <div className="slider" ref={sliderRef}>
+        {movies.map((movie) => (
+          <div className="longCard " key={movie?.id}>
+            <img
+              className="longCard__poster "
+              src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
+              alt={movie?.original_name}
+            />
+            <div className="longCard__info">
+              <div className="longCard__controls ">
+                <div className="longCard__info--wrapper">
+                  <button className="longCard__info--button">
+                    <BsFillPlayFill />
+                  </button>
+                  <button className="longCard__info--button">
+                    <FiPlus />
+                  </button>
                 </div>
-
-                <h2 className="movie__title">{item?.title}</h2>
-                <p className="movie__rate">
-                  {Math.floor(item?.vote_average * 10)}% Match
-                </p>
+                <div>
+                  <button className="longCard__info--button">
+                    <FiChevronDown className="arrow-down" />
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
 
-        <button onClick={slideRight} className="scroll__button  scroll__right">
-          <HiChevronRight />
-        </button>
+              <h2 className="longCard__title">{movie?.original_name}</h2>
+              <p className="longCard__rate">
+                {Math.floor(movie?.vote_average * 10)}% Match
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
-    </>
+      <button onClick={slideRight} className="scroll__button  scroll__right">
+        <VscChevronRight />
+      </button>
+    </div>
   );
 }
