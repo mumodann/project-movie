d7a607b52435f30fade37dae32b806eba04e4ca7
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-22 18:14:45 +0200

    recreate banner component

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -1,37 +1,26 @@
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { IoVolumeMediumOutline, IoVolumeOffOutline } from "react-icons/io5";
+import { BsFillPlayFill } from "react-icons/bs";
+import { BsVolumeUp, BsVolumeMute } from "react-icons/bs";
 import "/src/style/Banner.css";
 import ReactPlayer from "react-player";
 import { BiInfoCircle } from "react-icons/bi";
 
-export function Banner(): JSX.Element {
+export function Banner() {
   const [isMuted, setIsMuted] = useState<boolean>(true);
   return (
     <div className="banner">
       <ReactPlayer
         playing={true}
         loop={true}
-        volume={1}
-        muted={isMuted}
-        className="trailer__video"
         width="100%"
         height="100%"
+        volume={1}
+        muted={isMuted}
+        className="trailer"
         url="https://vimeo.com/329866666"
       />
-
-      {isMuted ? (
-        <button onClick={() => setIsMuted(false)} className="mute__button">
-          <IoVolumeOffOutline />
-        </button>
-      ) : (
-        <button onClick={() => setIsMuted(true)} className="mute__button">
-          <IoVolumeMediumOutline />
-        </button>
-      )}
       <article className="trailer__info">
-        <h1 className="trailer__title">The Lion King </h1>
+        <h1 className="trailer__title">The Lion King</h1>
         <p className="trailer__overview">
           Simba idolizes his father, King Mufasa, and takes to heart his own
           royal destiny. But not everyone in the kingdom celebrates the new
@@ -48,17 +37,31 @@ export function Banner(): JSX.Element {
           Simba idolizes his father, King Mufasa, and takes to heart his own
           royal destiny...
         </p>
-        <div className="trailer__container">
-          <button aria-label="Play" className="container__button">
-            <FontAwesomeIcon icon={faPlay} className="icon" />
-            &nbsp; Play
+        <div className="trailer__wrapper">
+          <button
+            onClick={() => alert("not a movie!")}
+            className="trailer__button primary"
+          >
+            <BsFillPlayFill className="trailer__button--icon" />
+            Play
           </button>
-          <button aria-label="My List" className="container__button">
-            <BiInfoCircle className="icon" />
-            &nbsp; More Info
+          <button className="trailer__button secondary">
+            <BiInfoCircle className="trailer__button--icon" />
+            More Info
           </button>
         </div>
-      </article>
+      </article>{" "}
+      {isMuted ? (
+        <button className="trailer__mute">
+          {" "}
+          <BsVolumeMute onClick={() => setIsMuted(false)} />
+        </button>
+      ) : (
+        <button className="trailer__mute">
+          <BsVolumeUp onClick={() => setIsMuted(true)} />
+        </button>
+      )}
+      <div className="banner__fade"></div>
     </div>
   );
 }
